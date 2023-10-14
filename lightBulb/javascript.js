a82b1650ee9e68c53277d39286a00cbb16b8ec8a
Antonio <147282894+filipovic-antonio@users.noreply.github.com>
2023-10-15 00:09:20 +0200

    Add files via upload

---
+++ lightBulb/javascript.js
@@ -0,0 +1,19 @@
+const lightBulbSwitch = document.querySelector('#lightBulbSwitch');
+const lightbulbImage = document.querySelector('#lightbulbImage');
+
+let isLightBulbOn = false;
+lightbulbImage.src ='lightOff.png'
+
+lightBulbSwitch.addEventListener('click', function(){
+    
+    if(isLightBulbOn) {
+        lightbulbImage.src ='lightOff.png'
+
+    isLightBulbOn= false;
+    } else {
+        lightbulbImage.src ='lightOn.png'
+  
+        isLightBulbOn= true;
+    } 
+    console.log(isLightBulbOn);
+});
